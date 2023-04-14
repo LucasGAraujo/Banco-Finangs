2a0db38ad7ffeef1b1dc6de64ff911dc942fd80b
Lucas Gomes <Lucasdag445@gmail.com>
2023-04-14 12:45:07 -0300

    Mensagem de alteração 1500px

---
+++ script.js
@@ -0,0 +1,16 @@
+function Scrollnavegar(){
+    
+    Form.addEventListener("click",() =>{
+        window.scrollTo({
+            top:1800,
+            behavior:"smooth"
+        })
+    })
+    Invest.addEventListener("click",() =>{
+        window.scrollTo({
+            top:900,
+            behavior:"smooth"
+        })
+    })
+    }
+Scrollnavegar();
